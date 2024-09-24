9e9a847145af92793581c90201076c23ede40fac
Jeelsavaliya <jeelsavaliya007@gmail.com>
2024-09-24 18:37:27 +0530

    Complete CRUD operation With Web API

---
+++ BlazorWebApp/Pages/AddStudent.razor.cs
@@ -0,0 +1,27 @@
+﻿using BlazorClass;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorWebApp.Pages
+{
+    public partial class AddStudent
+    {
+        [Inject]
+        public HttpClient http {  get; set; }
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+        public Student student { get; set; } = new();
+
+        public async Task Save()
+        {
+            http.PostAsJsonAsync("https://localhost:7041/api/Students", student);
+            StateHasChanged();
+            navigationManager.NavigateTo("/student-list", true);
+        }
+
+        public void BackToHome()
+        {
+            navigationManager.NavigateTo("/student-list",true);
+        }
+    }
+}
---
+++ BlazorWebApp/Pages/EditStudent.razor.cs
@@ -0,0 +1,35 @@
+﻿using BlazorClass;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorWebApp.Pages
+{
+    public partial class EditStudent
+    {
+        [Inject]
+        public HttpClient http { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+
+        public Student student { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            student = await http.GetFromJsonAsync<Student>($"https://localhost:7041/api/Students/{Id}");
+        }
+        public async Task UpdateStudnet()
+        {
+            await http.PutAsJsonAsync<Student>($"https://localhost:7041/api/Students/{student.Id}", student);
+            StateHasChanged();
+            navigationManager.NavigateTo("/student-list");
+        }
+        
+        public async Task BackToHome()
+        {
+            navigationManager.NavigateTo("/student-list");
+        }
+    }
+}
